dc14a7c7878bde8cf1f2e4eb45329db9dccca8f2
zjl7577 <1097919195@qq.com>
2018-08-14 10:56:20 +0800

    relation provider with videoPlayerDemo

--- app/src/main/java/com/example/zjl/myrecyclerview/activity/LockActivity.java
+++ app/src/main/java/com/example/zjl/myrecyclerview/activity/LockActivity.java
@@ -3,9 +3,13 @@ package com.example.zjl.myrecyclerview.activity;
 import android.app.Activity;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.zjl.myrecyclerview.R;
@@ -32,7 +36,51 @@ public class LockActivity extends Activity {
         }
 
         lockScreen(findViewById(R.id.btn));
+
+        initListener();
     }
+
+    private void initListener() {
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                addDataWithVideoPlayerProvider();
+            }
+        });
+    }
+
+    // https://www.jianshu.com/p/601086916c8f
+    private void addDataWithVideoPlayerProvider() {
+        Uri bookUri = Uri.parse("content://com.zjl.contentproviderdemo.BookProvider/book");
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("bookName", "叫什么名字好呢");
+        getContentResolver().insert(bookUri, contentValues);
+        Cursor bookCursor = getContentResolver().query(bookUri, new String[]{"_id", "bookName"}, null, null, null);
+        if (bookCursor != null) {
+            while (bookCursor.moveToNext()) {
+                Log.e("provider", "ID:" + bookCursor.getInt(bookCursor.getColumnIndex("_id"))
+                        + "  BookName:" + bookCursor.getString(bookCursor.getColumnIndex("bookName")));
+            }
+            bookCursor.close();
+        }
+
+        Uri userUri = Uri.parse("content://com.zjl.contentproviderdemo.BookProvider/user");
+        contentValues.clear();
+        contentValues.put("userName", "叶叶叶");
+        contentValues.put("sex", "男");
+        getContentResolver().insert(userUri, contentValues);
+        Cursor userCursor = getContentResolver().query(userUri, new String[]{"_id", "userName", "sex"}, null, null, null);
+        if (userCursor != null) {
+            while (userCursor.moveToNext()) {
+                Log.e("provider", "ID:" + userCursor.getInt(userCursor.getColumnIndex("_id"))
+                        + "  UserName:" + userCursor.getString(userCursor.getColumnIndex("userName"))
+                        + "  Sex:" + userCursor.getString(userCursor.getColumnIndex("sex")));
+            }
+            userCursor.close();
+        }
+    }
+
     //执行锁屏  http://gudong.name/2015/05/26/lock_screen.html
     public void lockScreen(View view){
         if (mDPM.isAdminActive(mAdminName)) {
